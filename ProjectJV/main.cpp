7582def32b0917e0ec96930efd616e11fd406f20
Joao Da Rocha <Joao Da Rocha@DESKTOP-QCP0MN5>
2019-10-18 10:44:15 +0200

    Ajoutez des fichiers projet.

---
+++ ProjectJV/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+class Complex
+{
+public:
+	Complex(double a, double b) : r(a), c(b) {}
+private:
+	double r; // Real Part
+	double c; // Complex Part
+};
+
+int main(int argc, char∗ argv[])
+{
+	Complex∗ array[1000];
+	for (int i = 0; i < 5000; i++) {
+		for (int j = 0; j < 1000; j++) {
+			array[j] = new Complex(i, j);
+		}
+		for (int j = 0; j < 1000; j++) {
+			delete array[j];
+		}
+	}
+	return 0;
+}
